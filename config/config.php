13fc3b85f13496878c2eef50c7849fbaa608ea9b
Maingron <r@maingron.com>
2025-08-14 14:44:07 +0200

    Add config.php

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+// Example config - will get merged with the default config in MailAddressTagger.php
+// You don't need to use a config file, as long as you pass some config variable to the MailAddressTagger constructor
+return [
+	'base_address' => 'contact@maingron.com',
+	'fallback_address' => 'contact+fallback@maingron.com'
+];
